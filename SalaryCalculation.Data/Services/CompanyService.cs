57c7d3f52547a11be3e726d070ba74c3fbb79e00
ZeljkoPetrovicVr <zeljkopetrovic83vr@gmail.com>
2021-05-11 09:04:08 +0200

    Add project files.

---
+++ SalaryCalculation.Data/Services/CompanyService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculation.Data.Services
+{
+    public class CompanyService
+    {
+        public static List<tbl_Employee> GetAllTbl_Employees()
+        {
+            using (CompanyEntities dbContext = new CompanyEntities())
+            {
+                return dbContext.tbl_Employee.ToList();
+            }								   
+        }
+
+        public static void CreateNewEmployee(tbl_Employee employee)
+        {
+            using (CompanyEntities dbContext = new CompanyEntities())
+            {
+                dbContext.tbl_Employee.Add(employee);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ WebApplication5/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SalaryCalculation.Data;
+using SalaryCalculation.Data.Services;
+
+namespace WebApplication5.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult ShowEmployees()
+        {
+            var employees = CompanyService.GetAllTbl_Employees();
+
+            return View(employees);
+        }
+
+        public ActionResult NewEmployee()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult NewEmployee(tbl_Employee employee)
+        {
+            employee.BrutSalary = CalculateBrutSalary(employee.NetSalary);
+
+            CompanyService.CreateNewEmployee(employee);
+
+            ViewBag.Message = "New employee " + employee.Name + " " + employee.Lastname +" record inserted successfully. " +
+                "Brut salary of the employee is: " + employee.BrutSalary;
+            ModelState.Clear();
+            return View();
+        }
+
+
+
+        private decimal CalculateBrutSalary(decimal netSalary)
+        {
+            decimal bruto_1 = (netSalary - 1830) / 0.701m; //(Plata - 1830(10% od neoporezivog iznosa))
+            decimal bruto_2 = bruto_1 * (decimal)1.1715;
+            return Math.Round(bruto_2, 2);
+        }
+    }
+}
